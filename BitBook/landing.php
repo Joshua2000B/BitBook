dadcdce642600006d4f5ff200e832368fe67457b
Elizabeth1998 <31674423+Elizabeth1998@users.noreply.github.com>
2019-11-21 15:36:57 -0500

    Add Files for front end development to start

---
+++ BitBook/landing.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html> 
+<body style="background-color: lightblue;">
+<div align="center">
+<br><br><br><br>
+
+
+
+<br><br><br><br>
+<hr width="50">
+<a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
+<hr width="50">
+</div>
+</body> </html>
