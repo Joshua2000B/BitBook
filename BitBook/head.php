43ec7197f1c2798a6711be0287752b5ab1f1c6f1
Elizabeth Hutchison <elizabeth68.h@gmail.com>
2019-11-22 17:55:24 -0500

    update the files

---
+++ BitBook/head.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> *Dynamic Page Title* </title>
+<meta name"description" "Dynamic Description">
+<link href="#">
+</head>
--- BitBook/landing.php
+++ BitBook/landing.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <html> 
+<head>
+<title> welcome to BitBook</title>
+<p> A new way to find and buy the books you love.</p>
+</head>
+
+<?php include('head.php'); ?>
+<?php include('nav-bar.php'); ?>
+
 <body style="background-color: lightblue;">
 <div align="center">
 <br><br><br><br>
@@ -11,4 +19,5 @@
 <a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
 <hr width="50">
 </div>
+<?php include('footer.php'); ?>
 </body> </html>
