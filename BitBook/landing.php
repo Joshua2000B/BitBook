450f8e4e3f02eff6f0079a1e99e0c5c3918d0c10
Elizabeth Hutchison <elizabeth68.h@gmail.com>
2019-11-23 12:47:09 -0500

    adjusting files and design for landing page

--- BitBook/landing.php
+++ BitBook/landing.php
@@ -1,23 +1,2 @@
 <!DOCTYPE html>
 <html> 
-<head>
-<title> welcome to BitBook</title>
-<p> A new way to find and buy the books you love.</p>
-</head>
-
-<?php include('head.php'); ?>
-<?php include('nav-bar.php'); ?>
-
-<body style="background-color: lightblue;">
-<div align="center">
-<br><br><br><br>
-
-
-
-<br><br><br><br>
-<hr width="50">
-<a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
-<hr width="50">
-</div>
-<?php include('footer.php'); ?>
-</body> </html>
