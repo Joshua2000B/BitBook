797b40b39a2443d16bb9cc012cfa1e08e068651f
fishingboatproceeded <42788817+fishingboatproceeded@users.noreply.github.com>
2019-11-28 22:24:12 -0500

    Trying to get search.php to work

--- BitBook/search.php
+++ BitBook/search.php
@@ -1,2 +1,16 @@
 <!DOCTYPE html>
 <html> 
+<body>
+
+<?php
+function get_books($term) {
+    $output = shell_exec("python \"../Bitbook\ Backend/search.py\" $term");
+    echo $output;
+}
+if(isset($_GET['search'])) {
+    get_books($_GET['search']);
+}
+?>
+
+</body>
+</html>
