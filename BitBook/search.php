dcd234a300f0d97e9e15f03fa424c067f7436d99
Joshua2000B <Joshua2000B@gmail.com>
2019-11-28 23:12:11 -0500

    Fixed search.php

--- BitBook/search.php
+++ BitBook/search.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<html> 
+<html>
 <body>
 
 <?php
 function get_books($term) {
-    $output = shell_exec("python ../Bitbook\ Backend/search.py $term");
-    echo $output;
+        $output = shell_exec("python3.8 ../BitBook\ Backend/search.py $term");
+        #echo gettype($output);
+        echo "<br>";
+        echo $output;
 }
+echo "OK";
 if(isset($_GET['search'])) {
+        echo $_GET['search'];
     get_books($_GET['search']);
 } else {
     echo "Search not set";
@@ -16,3 +20,4 @@ if(isset($_GET['search'])) {
 
 </body>
 </html>
+
