6eb89980aeb8818753e2125c81f7b9b00909e7ba
fishingboatproceeded <42788817+fishingboatproceeded@users.noreply.github.com>
2019-11-29 13:02:22 -0500

    Removed unnecessary lines and output from search.php

--- BitBook/search.php
+++ BitBook/search.php
@@ -5,17 +5,13 @@
 <?php
 function get_books($term) {
         $output = shell_exec("python3.8 ../BitBook\ Backend/search.py $term");
-        #echo gettype($output);
-        echo "<br>";
         echo $output;
 }
-echo "OK";
 if(isset($_GET['search'])) {
-        echo $_GET['search'];
     get_books($_GET['search']);
-} else {
-    echo "Search not set";
-}
+} #else {
+  #  echo "Search not set";
+  #}
 ?>
 
 </body>
