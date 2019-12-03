9585f911530fadf3418c3864a00a1f2816761406
Elizabeth Hutchison <elizabeth68.h@gmail.com>
2019-12-03 11:39:58 -0500

    order

--- BitBook/order.php
+++ BitBook/order.php
@@ -1,44 +1,3 @@
-<?php
-
-  if($_GET) {
-  echo "HEY";
-      if(isset($_GET['book'])) {
-
-          order($_GET['book']);
-      }
-  }
-
-  function order($book) {
-
-  $book = str_replace("+"," ",$book);
-
-  echo "IN FUNCTION with book: " . $book;
-  require("dbConnect.php");
-  if(isset($_COOKIE["user"])) {
-
-      $sql = "select isbn from book where title = '$book';";
-
-      $isbn = mysqli_query($conn,$sql);
-
-      if(! $isbn) {die('Could not enter data: ' . mysqli_error($conn));}
-
-      echo gettype($isbn);
-      $isbn = $isbn->fetch_row();
-      $time = date('Y-m-d G:i:s');
-
-      $sql = "insert into ordered values('$time','".$_COOKIE["user"]."',$isbn[0],0);";
-      echo $sql;
-      $retval = mysqli_query($conn,$sql);
-
-      if(! $retval) {die('Could not enter data: ' . mysqli_error($conn));}
-
-      mysqli_close($conn);
-  }
-  else {
-      echo "You aren't logged in!";
-  }
-  }
-?>
 <!DOCTYPE html>
 <html> 
     <link rel="stylesheet" href="BitBook.css">
@@ -56,37 +15,49 @@
 <body>
 
     <?php
-      
-    function get_books($term) {
-        $output = shell_exec("python3.8 ../BitBook\ Backend/search.py $term");
-        #echo "Output (type/contents): <br>";
-        #echo gettype($output) . "/|||" . $output . "|||";
-        #echo "<br>";
-        if($output == "{}\n") {
-          echo '<h1> Sorry! </h1>';
-          echo '<p> We were unable to find any books with those criteria, please try again </p>';
-          die();
+    
+  if($_GET) {
+    echo "HEY";
+        if(isset($_GET['book'])) {
+  
+            order($_GET['book']);
         }
-        $array = json_decode($output, true);
+    }
+  
+    function order($book) {
+  
+    $book = str_replace("+"," ",$book);
+  
+    echo "IN FUNCTION with book: " . $book;
+    require("dbConnect.php");
+    if(isset($_COOKIE["user"])) {
+  
+        $sql = "select isbn from book where title = '$book';";
+  
+        $isbn = mysqli_query($conn,$sql);
+  
+        if(! $isbn) {die('Could not enter data: ' . mysqli_error($conn));}
+  
+        echo gettype($isbn);
+        $isbn = $isbn->fetch_row();
+        $time = date('Y-m-d G:i:s');
+  
+        $sql = "insert into ordered values('$time','".$_COOKIE["user"]."',$isbn[0],0);";
+        echo $sql;
+        $retval = mysqli_query($conn,$sql);
+  
+        if(! $retval) {die('Could not enter data: ' . mysqli_error($conn));}
+  
+        mysqli_close($conn);
         foreach ($array as $book){ 
             echo '<div class="contentsBook" style="margin-top: 5%;">';
             echo '<div class="info" style="text-align: left; sans-serif; color: black; background-color:#2B7A78; border: 2px solid black; width: 45%; height: 500px; margin: auto;">';
             echo '<div class="topPart">';
-            echo '<div class="cover" style="margin: 25px; width: 45%; display:inline-block; vertical-align: top;">';
-            echo  '<img src="https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images-medium-5/sherlock-holmes-book-cover-poster-art-2-nishanth-gopinathan.jpg" alt="Cover Photo Did Not Load!" height="55%" width="45%;" float: right;>';
-            echo '</div>';
             echo '<div class="description" style="margin: 25px; display:inline-block;">';
-            echo '<p id="title">Title: ';
+            echo '<p>You have successfully ordered:';
             echo $book['title'];
             echo '</p>';
-
-	    echo '<form action="order.php" method="get">';
-	    echo '<button type="submit" name="book" value = ' . urlencode($book["title"]) . '>Purchase</button></form>';
-
-            echo '</div>';
-            echo '<div class="bottomPart" style="margin: 25px;">';
-            echo '<p id="summary">Summary: ';
-            echo $book['summary'];
+            echo '<p>Feel free to look for more books while you wait for your book to arrive!';
             echo '</p>';
             echo '</div>';
             echo '</div>';
@@ -94,9 +65,11 @@
             echo '</div>';
         }
     }
-    if(isset($_GET['search'])) {
-        get_books($_GET['search']);
+    else {
+        echo "You aren't logged in!";
     }
+    }
+
     ?>
 
 </body> 
